c00b053c8f08db9af18a20d9ea6fcdc4d7fdbd68
PRAZERES SANTOS Ilton Karly <ikprazeressantos@europe.corp.altran.com>
2020-04-21 12:11:56 +0100

    Finalizando o desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type is not accept');
+    }
+    if (type === 'outcome' && total < value) {
+      throw Error('Insufficient balance');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
